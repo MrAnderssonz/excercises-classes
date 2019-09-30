d28a6290e657864357eec0359aa7a24fdc245a63
Johan Andersson <ewan.andersson@gmail.com>
2019-09-30 13:39:55 +0200

    Klar med del övning 1 del 1

---
+++ Bongo.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace excercises_classes
+{
+    class Bongo
+    {
+        public string name = "";
+        public int age = 0;
+        public string race = "";
+
+        public void bark()
+        {
+            Console.WriteLine("NÖFF");
+        }
+        public void fetch()
+        {
+            Console.WriteLine("Jag har min egen vilja och tänker inte styras av dig!");
+        }
+
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace excercises_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bongo myBongo = new Bongo();
+            myBongo.name = "Sven";
+            myBongo.age = 2;
+            myBongo.race = "Brun";
+
+            Console.WriteLine("{0} heter min Bongo antilop, han är {1}år och är av rasen {2}.",myBongo.name, myBongo.age, myBongo.race);
+            myBongo.bark();
+            myBongo.fetch();
+
+        }
+    }
+}
